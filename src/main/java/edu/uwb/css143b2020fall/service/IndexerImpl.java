adf79ed4b4712ac84ba404f981b6eeca6e3f57c7
Chum <ravichum@gmail.com>
2020-12-08 09:44:03 -0800

    Indexer commit one

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -2,7 +2,6 @@ package edu.uwb.css143b2020fall.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +10,35 @@ import java.util.Map;
 public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
-        // add your code
+
+        int size = docs.size();
+
+        //wordMap the variable to store doc number and the place in doc.
+        Map<int[], String> wordMap = new HashMap<>();
+
+        for (int documentI = 0; documentI < size; documentI++) {
+
+            String[] words = docs.get(documentI).split(" ");
+
+            //getting rid of the spaces
+            int count = 0;
+            for (String word : words) {
+                if (word.equals(""))
+                    count++;
+
+                if (count != 0) {
+
+                    String[] newWords = new String[words.length - count];
+                    int newWordsIndex = 0;
+
+                    for (int wordsIndex = 0; wordsIndex < words.length; wordsIndex++) {
+
+                    }
+                }
+
+
+            }
+        }
         return indexes;
     }
 }
